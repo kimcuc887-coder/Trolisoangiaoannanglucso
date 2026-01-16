9e5017f2964ffdc5f0c1b72a7f0558e1d832b953
kimcuc887-coder <kimcuc887@gmail.com>
2026-01-16 14:25:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Nguyễn Thị Kim Cúc - Trường Tiểu học Quyết Tiến - ĐT: 098 4201 487</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 -  Nguyễn Thị Kim Cúc - Trường Tiểu học Quyết Tiến - ĐT: 098 4201 487</div>", unsafe_allow_html=True)
